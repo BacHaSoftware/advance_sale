d388524df8daf4f97a5fa4f1a96c0a18e8d669d9
“YenTran” <yentran.bhtech@gmail.com>
2026-03-31 17:40:32 +0700

    [IMP] Upgrade module bhs_sales ver 19

--- bhs_sales/models/sale_order.py
+++ bhs_sales/models/sale_order.py
@@ -10,35 +10,8 @@ class SaleOrderLine(models.Model):
 
     bhs_project_id = fields.Many2one('project.project', string="BHS Project")
 
-    @api.depends('product_id', 'product_uom', 'product_uom_qty')
-    def _compute_price_unit(self):
-        for line in self:
-            # check if there is already invoiced amount. if so, the price shouldn't change as it might have been
-            # manually edited
-            if line.qty_invoiced > 0:
-                continue
-            # check if option change_line_by_pricelist not checked
-            # manually edited
-            if not line.order_id.change_line_by_pricelist:
-                continue
-            if not line.product_uom or not line.product_id:
-                line.price_unit = 0.0
-            else:
-                price = line.with_company(line.company_id)._get_display_price()
-                line.price_unit = line.product_id._get_tax_included_unit_price(
-                    line.company_id or line.env.company,
-                    line.order_id.currency_id,
-                    line.order_id.date_order,
-                    'sale',
-                    fiscal_position=line.order_id.fiscal_position_id,
-                    product_price_unit=price,
-                    product_currency=line.currency_id
-                )
-
 class SaleOrder(models.Model):
     _inherit = "sale.order"
-
-    change_line_by_pricelist = fields.Boolean(string='Change line by Pricelist?', default=True)
 
     amount_total_main = fields.Monetary(
         string='Total in main currency',
